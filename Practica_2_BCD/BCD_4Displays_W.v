1334993395679453e2a05f17e479e2758bcc04c2
AlejandroXieHu <a01743620@tec.mx>
2026-02-19 17:54:04 -0600

    BCD_4Displays_W.v

---
+++ Practica_2_BCD/BCD_4Displays_W.v
@@ -0,0 +1,10 @@
+module BCD_4Displays_W (
+
+    input  [9:0] SW,
+    output [6:0] HEX0, HEX1, HEX2, HEX3
+    
+);
+
+    BCD_4Displays WRAP (.bcd_in(SW), .D_un(HEX0), .D_de(HEX1), .D_ce(HEX2), .D_mi(HEX3));
+
+endmodule
