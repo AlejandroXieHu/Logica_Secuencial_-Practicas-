3b978eb9914b1e564aa9b9beeac472e9a7650f36
AlejandroXieHu <a01743620@tec.mx>
2026-02-19 17:53:30 -0600

    BCD_4Displays_tb.v

---
+++ Practica_2_BCD/BCD_4Displays_tb.v
@@ -0,0 +1,29 @@
+module BCD_4Displays_tb();
+
+    reg  [9:0] bcd_in;
+    wire [6:0] D_un, D_de, D_ce, D_mi;
+    wire [3:0] unidades, decenas, centenas, millares;
+
+    BCD_4Displays dut(.bcd_in(bcd_in), .D_un(D_un), .D_de(D_de), .D_ce(D_ce), .D_mi(D_mi), .unidades(unidades), .decenas(decenas), .centenas(centenas), .millares(millares));
+
+    initial 
+        begin
+            repeat (10)
+            begin
+                bcd_in = $random % 1024; #10;
+            end
+                $finish;
+        end
+
+    initial 
+        begin
+            $monitor("bcd_in = %d, Unidades = %d, Decenas = %d, Centenas = %d, Millares = %d", bcd_in, unidades, decenas, centenas, millares);
+        end
+
+    initial 
+        begin
+            $dumpfile("BCD_4Displays_tb.vcd");
+            $dumpvars(0, BCD_4Displays_tb);
+        end
+        
+endmodule
