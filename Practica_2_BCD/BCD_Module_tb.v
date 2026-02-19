5887159d28c5ba9a1a5140e939ba5ae05c90003f
AlejandroXieHu <a01743620@tec.mx>
2026-02-19 17:52:36 -0600

    BCD_Module_tb.v

---
+++ Practica_2_BCD/BCD_Module_tb.v
@@ -0,0 +1,28 @@
+module BCD_module_tb();
+
+    reg [3:0] bcd_in;
+    wire [6:0] bcd_out;
+
+    BCD_module dut(.bcd_in(bcd_in), .bcd_out(bcd_out));
+
+    initial 
+        begin
+            repeat (32) // 32 es el número de iteraciones de las repeticiones
+            begin
+                bcd_in = $random % 16; #10; // $random es para generar un número aleatorio y el % 16 es para dividir el número entre 16 y nos daría un número menor que 16
+            end
+                $finish;
+        end
+
+    initial 
+        begin
+            $monitor("bcd_in = %b, bcd_out = %b", bcd_in, bcd_out);
+        end
+
+    initial 
+        begin
+            $dumpfile("BCD_module_tb.vcd");
+            $dumpvars(0, BCD_module_tb);
+        end
+
+endmodule
