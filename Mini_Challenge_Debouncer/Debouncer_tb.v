00080f989c9c2d5b9e7c0036e17d2690183616c9
AlejandroXieHu <a01743620@tec.mx>
2026-03-04 07:21:19 -0600

    Debouncer_tb.v

---
+++ Mini_Challenge_Debouncer/Debouncer_tb.v
@@ -0,0 +1,57 @@
+module debouncer_tb;
+
+    reg clk;
+    reg [1:0] KEY;
+    wire [9:0] LEDR;
+
+    debouncer_wr DUT (
+        .MAX10_CLK1_50(clk),
+        .KEY(KEY),
+        .LEDR(LEDR)
+    );
+
+    initial
+        clk = 0;
+
+    always
+        #10 clk = ~clk;
+
+    initial
+    begin
+        KEY = 2'b11;
+
+        $display("Inicio de simulación");
+        $monitor("Tiempo=%0t | KEY=%b | Contador=%d", $time, KEY, LEDR);
+
+        #100;
+
+        KEY[1] = 0;
+        #21000000;
+        KEY[1] = 1;
+        #21000000;
+
+        $display("Primera presión");
+
+        KEY[0] = 0;
+        #21000000;
+        KEY[0] = 1;
+        #21000000;
+
+        $display("Segunda presión");
+
+        KEY[0] = 0;
+        #21000000;
+        KEY[0] = 1;
+        #21000000;
+
+        $display("Reset");
+
+        KEY[1] = 0;
+        #21000000;
+        KEY[1] = 1;
+        #21000000;
+
+        $display("Fin de simulación");
+        $finish;
+    end
+endmodule
